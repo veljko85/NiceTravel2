632d0b9dbdff643591e66cb8d2cef8faa2362015
Velibor Markovic <veljko85@users.noreply.github.com>
2018-11-14 17:39:50 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -22,26 +22,6 @@ $(document).ready(function(){
 	});
 
 
-	$(".navBar a").click(function(){
 
-
-		$(".line1").toggleClass("change1");
-		$(".line2").toggleClass("change2");
-
-		$(".navBar").slideToggle(300);
-
-		$(".homeSection").hide();
-
-		$(".section").fadeIn();
-
-		$(".section").load($(this).attr("href")).hide().fadeIn();
-        	return(false);
-		
-
-
-
-
-		
-	});
 
 });
--- js/side.js
+++ js/side.js
@@ -5,4 +5,26 @@ $(document).ready(function()	{
             	return(false);
 
     }); 
+
+    	$(".navBar a").click(function(){
+
+
+		$(".line1").toggleClass("change1");
+		$(".line2").toggleClass("change2");
+
+		$(".navBar").slideToggle(300);
+
+		$(".homeSection").hide();
+
+		$(".section").fadeIn();
+
+		$(".section").load($(this).attr("href")).hide().fadeIn();
+        	return(false);
+		
+
+
+
+
+		
+	});
 }); 
