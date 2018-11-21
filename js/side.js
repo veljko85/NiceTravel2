b4681bc5fd238539ff1ba18a9c11a43d6c171dc4
Velibor Markovic <veljko85@users.noreply.github.com>
2018-11-21 18:30:25 +0100

    Update side.js

--- js/side.js
+++ js/side.js
@@ -21,8 +21,6 @@ $(document).ready(function()	{
 
 		$(".section").show();
 
-		$(".load-btn-wrap").show();
-	    $(".load-btn-wrap").delay(700).fadeOut();
 
 	   	$(".section2").load($(this).attr("href")).hide().fadeIn();
         	return(false);
