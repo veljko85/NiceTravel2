7c740070c7f9a572c1b7c1474502ba4ea88f1885
Velibor Markovic <veljko85@users.noreply.github.com>
2018-12-14 16:10:55 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -32,6 +32,15 @@ $(document).ready(function(){
 		$(".line1").toggleClass("change1");
 		$(".line2").toggleClass("change2");
 		$(".navBar").slideToggle();
+
+		$(".section").show();	
+		
+		$(".load-btn-wrap").show();
+	        $(".load-btn-wrap").delay(700).fadeOut();
+
+	        $(".section2").load($(this).attr("href")).hide().fadeIn();
+            	return(false); 
+
 	});	
 
 
