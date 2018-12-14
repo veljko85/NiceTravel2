a8a78a19afa85892edac47802fc1c277010be0cd
Velibor Markovic <veljko85@users.noreply.github.com>
2018-12-14 15:47:03 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 	$(".homeSection").load("templates/homeSection.html"); 
 	$(".bottom").load("templates/bottom.html"); 
 	$(".ct p").fadeIn(2000);
-$(".navBar").load("templates/navBar.html"); 
+	$(".navBar").load("templates/navBar.html"); 
 
 
 
@@ -29,12 +29,6 @@ $(".navBar").load("templates/navBar.html");
 	});
 
 
-	$(".navBar a").click(function(){
-
-		$(".line1").toggleClass("change1");
-		$(".line2").toggleClass("change2");
-		$(".navBar").slideUp();
-	});	
 
 
 		
