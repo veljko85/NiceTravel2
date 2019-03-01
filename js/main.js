76c6d45414696894bf460281241ca72d70c2ad1c
Velibor Markovic <veljko85@users.noreply.github.com>
2019-03-01 11:38:52 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -49,15 +49,6 @@ if (document.body.clientWidth > 1200){
 	});	
 }
 	
-	$(".navBar a").click(function(){	
-		$(".section").show();	
-		
-		$(".load-btn-wrap").show();
-	        $(".load-btn-wrap").delay(700).fadeOut();
-
-	        $(".section2").load($(this).attr("href")).hide().fadeIn();
-            	return(false); 	
-    });
 
 });
 
